f312b1176a4eb6593fe1dc179d2340490adda47e
John <user@John-Pooleys-Macbook.local>
2020-02-26 11:42:45 +0000

    show index working on server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express =  require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client)=>{
+  const db = client.db('hotel_bookings');
+  const bookingsCollection = db.collection('bookings');
+  const bookingsRouter = createRouter(bookingsCollection);
+  app.use('/api/bookings', bookingsRouter);
+})
+.catch(console.err);
+
+app.listen(3000, function(){
+  console.log(`listening on port ${this.address().port }`);
+});
